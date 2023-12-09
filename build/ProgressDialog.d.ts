76264c39e757771e48de84ded8c6cdb644ff9619
GermanBluefox <dogafox@gmail.com>
2023-12-09 19:25:53 +0100

    Added some field for DeviceInfo

---
+++ build/ProgressDialog.d.ts
@@ -0,0 +1,9 @@
+export interface ProgressDialog {
+    update(update: {
+        title?: ioBroker.StringOrTranslated;
+        indeterminate?: boolean;
+        value?: number;
+        label?: ioBroker.StringOrTranslated;
+    }): Promise<void>;
+    close(): Promise<void>;
+}
